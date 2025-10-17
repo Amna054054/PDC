56dcf017bb85490b2127cecd12c4caf6aeb4eb90
bibi-amna <amna.riaz054054@gmail.com>
2025-10-17 23:06:56 +0500

    applied the sequential process time on the code

---
+++ Activity1.py
@@ -0,0 +1,10 @@
+import time
+
+print("Task 1: Starting")
+time.sleep(2)   # wait 2 seconds
+print("Task 1: Done")
+
+print("Task 2: Starting")
+time.sleep(2)   # wait 2 seconds
+print("Task 2: Done")
+
